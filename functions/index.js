5d47983f44fd3a6ce8854a9287af4b4c835e9f88
Anass ELABBADI <anasselabbadi.pro@gmail.com>
2019-11-10 16:53:13 +0100

    Authentication middleware

--- functions/index.js
+++ functions/index.js
@@ -39,10 +39,38 @@ app.get("/screams", (req, res) => {
         .catch(err => console.error(err));
 });
 
-app.post("/scream", (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        return res.status(403).json({ error: 'Unauthorized' })
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedIdToken => {
+            req.user = decodedIdToken;
+            console.log(decodedIdToken);
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token ', err);
+            return res.status(403).json(err); 
+        })
+};
+
+// Post one scream
+app.post("/scream", FBAuth, (req, res) => {
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
