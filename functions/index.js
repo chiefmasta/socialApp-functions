66e7bcbf91eb2795cbe8f7797d63bcc6fabc98ae
Anass ELABBADI <anasselabbadi.pro@gmail.com>
2019-11-12 14:24:24 +0100

    Like and Unlike functions added

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,38 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-const app = require("express")();
+const app = require('express')();
 
-const fbAuth = require("./utilities/fbAuth");
+const fbAuth = require('./utilities/fbAuth');
 
 const {
     getAllScreams,
     postOneScream,
     getScream,
-    commentOnScream
-} = require("./handlers/screams");
+    likeScream,
+    unlikeScream,
+    commentOnScream,
+} = require('./handlers/screams');
 const {
     signUp,
     signIn,
     uploadImage,
     addUserDetails,
-    getAuthenticatedUser
-} = require("./handlers/users");
+    getAuthenticatedUser,
+} = require('./handlers/users');
 
 // Scream routes
-app.get("/screams", getAllScreams);
-app.post("/scream", fbAuth, postOneScream);
-app.get("/scream/:screamId", getScream);
-app.post("/scream/:screamId/comment", fbAuth, commentOnScream);
+app.get('/screams', getAllScreams);
+app.post('/scream', fbAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+app.get('/scream/:screamId/like', fbAuth, likeScream);
+app.get('/scream/:screamId/unlike', fbAuth, unlikeScream);
+app.post('/scream/:screamId/comment', fbAuth, commentOnScream);
 
 // Signup Route
-app.post("/signup", signUp);
-app.post("/login", signIn);
-app.post("/user/image", fbAuth, uploadImage);
-app.post("/user", fbAuth, addUserDetails);
-app.get("/user", fbAuth, getAuthenticatedUser);
+app.post('/signup', signUp);
+app.post('/login', signIn);
+app.post('/user/image', fbAuth, uploadImage);
+app.post('/user', fbAuth, addUserDetails);
+app.get('/user', fbAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
